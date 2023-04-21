097a3e9dbae8b199b6395ccdf03d12b4736fa37e
Charliie2003 <122263939+Charliie2003@users.noreply.github.com>
2023-04-21 10:50:59 +0200

    algo2

--- app/src/main/java/com/xavifortes/finderfiesta/MainActivity.kt
+++ app/src/main/java/com/xavifortes/finderfiesta/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
