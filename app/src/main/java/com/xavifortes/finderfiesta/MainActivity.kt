51565c0a7c9abea5009a94c9da26e53fc0f25a42
xavifortes <itsfortes@gmail.com>
2023-04-21 10:18:00 +0200

    a

--- app/src/main/java/com/xavifortes/finderfiesta/MainActivity.kt
+++ app/src/main/java/com/xavifortes/finderfiesta/MainActivity.kt
@@ -2,10 +2,19 @@ package com.xavifortes.finderfiesta
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        val bLogin = findViewById<Button>(R.id.iniciar_ses)
+        bLogin.setOnClickListener {
+            // Code to be executed when the button is clicked
+            // For example, show a toast message
+            Toast.makeText(this, "Button clicked!", Toast.LENGTH_SHORT).show()
+            
+        }
     }
 }
